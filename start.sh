d52c9a83082f8884d08d9f5647cb497586bc9520
chh <522024330002@smail.nju.edu.cn>
2025-09-12 23:43:32 +0800

    feat: add taichi python aot runtime

---
+++ start.sh
@@ -0,0 +1,3 @@
+export TAICHI_CMAKE_ARGS="-DTI_WITH_VULKAN:BOOL=ON -DTI_WITH_CUDA:BOOL=ON -DTI_WITH_C_API:BOOL=ON"
+export TI_LIB_DIR=/home/chh/taichi/python/taichi/_lib/runtime                                     
+./build.py --shell  
